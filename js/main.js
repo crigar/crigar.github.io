596d2e23c706773c49716f280045970d530faa77
Cristian Garcia <cragarciapr@unal.edu.co>
2018-09-01 17:11:06 -0500

    asdf

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Estudiante", "Desarrollador de aplicaciones Web.", "Deportista"],
       typeSpeed: 100,
       loop: true,
     });
